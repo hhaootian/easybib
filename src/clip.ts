f96520e0a79e02e041c7b2a422c6ef0ebe118f64
htian97 <haot@mail.smu.edu>
2021-12-20 10:23:47 -0600

    new feature added
    clip can paste citekey to cursor

--- src/clip.ts
+++ src/clip.ts
@@ -10,13 +10,23 @@ function clip() {
             if (workSpace && workSpace[0]) {
                 // currently hardcode bib filename
                 let filePath = workSpace[0].uri.path + "/ref.bib";
-                
+
                 const fs = require('fs');
                 fs.appendFileSync(filePath, `\n${clipboardContent}\n`);
 
                 // pop up notice
                 let title = clipboardContent.split(",")[0].split("{")[1];
-                vscode.window.showInformationMessage("added: " + title);
+                // no need for notice
+                // vscode.window.showInformationMessage("added: " + title);
+
+                let textEditor = vscode.window.activeTextEditor;
+                if (textEditor !== undefined) {
+                    let uri = textEditor.document.uri;
+                    let edit = new vscode.WorkspaceEdit();
+
+                    edit.insert(uri, textEditor.selection.active, title);
+                    vscode.workspace.applyEdit(edit);
+                }
             }
         }
     );
--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,11 @@ import clip from './clip';
 import searchBox from './search';
 
 export function activate(context: vscode.ExtensionContext) {
-	let clipCommand = vscode.commands.registerCommand('autobib.clip', () => clip());
-	let searchCommand = vscode.commands.registerCommand("autobib.search", () => searchBox());
+    let clipCommand = vscode.commands.registerCommand('autobib.clip', () => clip());
+    let searchCommand = vscode.commands.registerCommand("autobib.search", () => searchBox());
 
-	context.subscriptions.push(clipCommand);
-	context.subscriptions.push(searchCommand);
+    context.subscriptions.push(clipCommand);
+    context.subscriptions.push(searchCommand);
 }
 
 export function deactivate() {}
